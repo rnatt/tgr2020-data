02577c8a4875c99e9557f7323fd0059c879b7635
root <root@DESKTOP-A5R9NQP.localdomain>
2020-01-09 21:29:33 +0700

    update

--- Model.js
+++ Model.js
@@ -28,10 +28,15 @@ async function createData(filename){
   const dataset = tf.data.csv('file://'+filename, {hasHeader: true});
   const v = await dataset.toArray();
     v.forEach( (data) => {
-        if(data.topic=='/tgr2020/jan08/data/121'){mac=[...mac,[0,parseInt(data.rssi, 10)]];}
+        if(data.topic=='/tgr2020/jan08/data/12'){mac=[...mac,[0,parseInt(data.rssi, 10)]];}
         else if(data.topic=='/tgr2020/jan08/data/15'){mac=[...mac,[1,parseInt(data.rssi, 10)]];}
         else if(data.topic=='/tgr2020/jan08/data/30'){mac=[...mac,[2,parseInt(data.rssi, 10)]];}
         else if(data.topic=='/tgr2020/jan08/data/33'){mac=[...mac,[3,parseInt(data.rssi, 10)]];}
+
+        if(data.topic=='/tgr2020/track/data/12'){mac=[...mac,[0,parseInt(data.rssi, 10)]];}
+        else if(data.topic=='/tgr2020/track/data/15'){mac=[...mac,[1,parseInt(data.rssi, 10)]];}
+        else if(data.topic=='/tgr2020/track/data/30'){mac=[...mac,[2,parseInt(data.rssi, 10)]];}
+        else if(data.topic=='/tgr2020/track/data/33'){mac=[...mac,[3,parseInt(data.rssi, 10)]];}
 
         if(data.mac_addr=='80:E1:26:07:E3:79'){label1=[...label1,0];}
         else if(data.mac_addr=='80:E1:26:07:C4:0A'){label1=[...label1,1];}
@@ -40,33 +45,35 @@ async function createData(filename){
         else{label1=[...label1,0];}
         
     });
+    console.log(mac)
+    console.log(label1)
     label1.forEach(function(data, i)
-    {if(data==0)
-      {if(mac[i][0]==0);g120=[...g120,...[mac[i][1]]];}
-      {if(mac[i][0]==1);g150=[...g150,...[mac[i][1]]];}
-      {if(mac[i][0]==2);g300=[...g300,...[mac[i][1]]];}
-      {if(mac[i][0]==3);g330=[...g330,...[mac[i][1]]];}
+    {if(data==0){
+      if(mac[i][0]==0){g120=[...g120,mac[i][1]];}
+      if(mac[i][0]==1){g150=[...g150,mac[i][1]];}
+      if(mac[i][0]==2){g300=[...g300,mac[i][1]];}
+      if(mac[i][0]==3){g330=[...g330,mac[i][1]];}}
     });
     label1.forEach(function(data, i)
-    {if(data==1)
-      {if(mac[i][0]==0);g121=[...g121,...[mac[i][1]]];}
-      {if(mac[i][0]==1);g151=[...g151,...[mac[i][1]]];}
-      {if(mac[i][0]==2);g301=[...g301,...[mac[i][1]]];}
-      {if(mac[i][0]==3);g331=[...g331,...[mac[i][1]]];}
+    {if(data==1){
+      if(mac[i][0]==0){g121=[...g121,[mac[i][1]]];}
+      if(mac[i][0]==1){g151=[...g151,[mac[i][1]]];}
+      if(mac[i][0]==2){g301=[...g301,[mac[i][1]]];}
+      if(mac[i][0]==3){g331=[...g331,[mac[i][1]]];}}
     });
     label1.forEach(function(data, i)
-    {if(data==2)
-      {if(mac[i][0]==0);g122=[...g122,...[mac[i][1]]];}
-      {if(mac[i][0]==1);g152=[...g152,...[mac[i][1]]];}
-      {if(mac[i][0]==2);g302=[...g302,...[mac[i][1]]];}
-      {if(mac[i][0]==3);g332=[...g332,...[mac[i][1]]];}
+    {if(data==2){
+      if(mac[i][0]==0){g122=[...g122,mac[i][1]];}
+      if(mac[i][0]==1){g152=[...g152,mac[i][1]];}
+      if(mac[i][0]==2){g302=[...g302,mac[i][1]];}
+      if(mac[i][0]==3){g332=[...g332,mac[i][1]];}}
     });
     label1.forEach(function(data, i)
-    {if(data==3)
-      {if(mac[i][0]==0);g123=[...g123,...[mac[i][1]]];}
-      {if(mac[i][0]==1);g153=[...g153,...[mac[i][1]]];}
-      {if(mac[i][0]==2);g303=[...g303,...[mac[i][1]]];}
-      {if(mac[i][0]==3);g333=[...g333,...[mac[i][1]]];}
+    {if(data==3){
+      if(mac[i][0]==0){g123=[...g123,mac[i][1]];}
+      if(mac[i][0]==1){g153=[...g153,mac[i][1]];}
+      if(mac[i][0]==2){g303=[...g303,mac[i][1]];}
+      if(mac[i][0]==3){g333=[...g333,mac[i][1]];}}
     });
     a = Math.min(g120.length,g150.length,g300.length,g330.length);
     b = Math.min(g121.length,g151.length,g301.length,g331.length);
@@ -75,24 +82,83 @@ async function createData(filename){
 
     a = Math.min(a,b,c,d);
     console.log(a);
-    for (var i =0;i<a;i++){
+    console.log(g120);
+    console.log(g150);
+    console.log(g300);
+    console.log(g330);
+    console.log('1');
+    console.log(g121);
+    console.log(g151);
+    console.log(g301);
+    console.log(g331);
+    console.log('2');
+    console.log(g122);
+    console.log(g152);
+    console.log(g302);
+    console.log(g332);
+    console.log('3');
+    console.log(g123);
+    console.log(g153);
+    console.log(g303);
+    console.log(g333);
+
+
+    for (var i =0; i<(100-g120.length); i){
+       g120.push(-Math.floor((Math.random()*(67-37+1))+37));}
+
+    for (var i =0;i<100-g150.length;i){
+      g150.push(-Math.floor((Math.random()*(81-60+1))+60));}
+    for (var i =0;i<100-g300.length;i){
+      g300.push(-Math.floor((Math.random()*(88-66+1))+66));}
+    for (var i =0;i<100-g330.length;i){
+      g330.push(-Math.floor((Math.random()*(85-74+1))+74));}
+
+    for (var i =0;i<100-g121.length;i){
+      g121.push(-Math.floor((Math.random()*(82-60+1))+60));}
+    for (var i =0;i<100-g151.length;i){
+      g151.push(-Math.floor((Math.random()*(43-41+1))+41));}
+    for (var i =0;i<100-g301.length;i){
+      g301.push(-Math.floor((Math.random()*(80-73+1))+73));}
+    for (var i =0;i<100-g331.length;i){
+      g331.push(-Math.floor((Math.random()*(78-60+1))+60));}
+
+    for (var i =0;i<100-g122.length;i){
+      g122.push(-Math.floor((Math.random()*(85-71+1))+71));}
+    for (var i =0;i<100-g152.length;i){
+      g152.push(-Math.floor((Math.random()*(81-58+1))+58));}
+    for (var i =0;i<100-g302.length;i){
+      g302.push(-Math.floor((Math.random()*(56-30+1))+30));}
+    for (var i =0;i<100-g332.length;i){
+      g332.push(-Math.floor((Math.random()*(85-80+1))+80));}
+
+    for (var i =0;i<100-g123.length;i){
+      g123.push(-Math.floor((Math.random()*(85-76+1))+76));}
+    for (var i =0;i<100-g153.length;i){
+      g153.push(-Math.floor((Math.random()*(77-65+1))+65));}
+    for (var i =0;i<100-g303.length;i){
+      g303.push(-Math.floor((Math.random()*(84-71+1))+71));}
+    for (var i =0;i<100-g333.length;i){
+      g333.push(-Math.floor((Math.random()*(69-60+1))+60));}
+      
+    
+    for (var i =0;i<100;i++){
       dac=[...dac,[g120[i],g150[i],g300[i],g330[i]]]
       label=[...label,0]}
     
   
-    for (var i =0;i<a;i++){
+    for (var i =0;i<100;i++){
         dac=[...dac,[g121[i],g151[i],g301[i],g331[i]]]
         label=[...label,1]}
     
-    for (var i =0;i<a;i++){
+    for (var i =0;i<100;i++){
       dac=[...dac,[g122[i],g152[i],g302[i],g332[i]]]
       label=[...label,2]}
 
     
-    for (var i =0;i<a;i++){
+    for (var i =0;i<100;i++){
       dac=[...dac,[g123[i],g153[i],g303[i],g333[i]]]
       label=[...label,3]}
-      
+    console.log(dac);
     
     return {dac,label};
 }
@@ -139,7 +205,7 @@ async function predictModel(xv){
 }
 
 const test = async()=>{
-  temp = await createData('data2.csv');
+  temp = await createData('data3.csv');
   //console.log(temp.dac);
   //console.log(temp.label);
   x = tf.tensor2d(temp.dac);
@@ -148,9 +214,9 @@ const test = async()=>{
   y = tf.oneHot(temp.label,4);
   y.print();
   model = await createModel();
-  //y = await trainModel(model,x,y,200);
+  y = await trainModel(model,x,y,200);
  // x =tf.tensor2d([[1,-80]])
-  x = tf.tensor2d([[ -50, -80, -10, -50 ]]);
+  x = tf.tensor2d([[ -47, -70, -66, -77 ]]);
   y = await predictModel(x);
   predict = class_name[y];
   console.log(predict);
--- predictModel.js
+++ predictModel.js
@@ -1,6 +1,9 @@
 const tf = require('@tensorflow/tfjs-node');
-
+const http = require('http');
+const request = require('request')
 const class_name = ['A','B','C','D'];
+
+
 
 async function predictModel(xv){
   const model = await tf.loadLayersModel('file://./mymodel/model.json');
@@ -12,15 +15,57 @@ async function predictModel(xv){
   l = [...l,v[2]];
   l = [...l,v[3]];
   a=l.indexOf(Math.max(...l));
-  console.log(l);
+  //console.log(l);
   return a;
 }
 
-const test = async()=>{
-  x = tf.tensor2d([[ -50, -80, -10, -50 ]]);
-  y = await predictModel(x);
-  predict = class_name[y];
-  console.log(predict);
 
+
+function call1(){
+  setTimeout(() => {
+http.get('http://202.139.192.184/api/tr25lt', (response) => {
+  let todo = '';
+  var predict= 'A';
+  response.on('data', (chunk) => {
+    todo += chunk;
+  });
+
+  // called when the complete response is received.
+  response.on('end', () => {
+    //todo.rssi
+    var v = JSON.parse(todo);
+    var x = [[-100,-100,-100,-100]];
+    if(v.ss12 !==null){x[0][0] = v.ss12.rssi;}
+    if(v.ss15 !==null){x[0][1] = v.ss15.rssi;}
+    if(v.ss30 !==null){x[0][2] = v.ss30.rssi;}
+    if(v.ss33 !==null){x[0][3] = v.ss33.rssi;}
+    const test = async()=>{
+      x = await tf.tensor2d(x);
+      y = await predictModel(x);
+      predict = class_name[y];
+      console.log(predict);
+      request.post('http://202.139.192.184/api/datapost', {
+      json: {
+      predict: predict
+      }
+      }, (error, res, body) => {
+      if (error) {
+      console.error(error)
+      return
+    }
+    console.log(`statusCode: ${res.statusCode}`)
+    console.log(body)
+})
+    }
+    test();
+    
+  });
+
+}).on("error", (error) => {
+  console.log("Error: " + error.message);
+});},2000);
+  
 }
-test();
+
+let i = 0;
+setInterval(() => {call1()},2000);
